80abdacd54f2eb29369b5ff80f656a732d62c628
Shreya <shreyaagoyall.08@gmail.com>
2026-09-18 09:34:58 +0530

    Fixed broken frontend config files, added routing with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,23 @@
-﻿export default function App() {
-  return <div>VoiceLedger</div>;
+﻿import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Ledger from "./pages/Ledger.jsx";
+import Ask from "./pages/Ask.jsx";
+import Risk from "./pages/Risk.jsx";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/ledger">Ledger</Link> |{" "}
+        <Link to="/ask">Ask</Link> | <Link to="/risk">Risk</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ledger" element={<Ledger />} />
+        <Route path="/ask" element={<Ask />} />
+        <Route path="/risk" element={<Risk />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
